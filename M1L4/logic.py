aa18c3bbe59635e7a705a811efaf5569c10fbb0b
vadim-and <162820240+vadim-and@users.noreply.github.com>
2025-08-17 20:39:07 +0500

    Update logic.py

--- M1L4/logic.py
+++ M1L4/logic.py
@@ -1,69 +1,74 @@
+from random import randint
+import requests
+
 class Pokemon:
-    pokemons = {}
-    BASE_HEALTH = 100 
-    BASE_ATTACK = 20
+    pokemons = {} # { username : pokemon}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
 
-    def __init__(self, pokemon_trainer): 
-
-        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_trainer = pokemon_trainer   
 
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-        self.health = Pokemon.BASE_HEALTH 
-        self.attack = Pokemon.BASE_ATTACK 
+
+        self.power = randint(30, 60)
+        self.hp = randint(200, 400)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
+    # Метод для получения картинки покемона через API
     def get_img(self):
-        url = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{self.pokemon_number}.png'
-        try:
-            response = requests.get(url)
-           
-
-         
-            return url  
-        except requests.exceptions.RequestException as e:
-            print(f"Ошибка при загрузке изображения: {e}")
-            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']["other"]['official-artwork']["front_default"])
+        else:
+            return "https://static.wikia.nocookie.net/anime-characters-fight/images/7/77/Pikachu.png/revision/latest/scale-to-width-down/700?cb=20181021155144&amp;amp;amp;path-prefix=ru"
     
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        try:
-            response = requests.get(url)
-            response.raise_for_status() 
+        response = requests.get(url)
+        if response.status_code == 200:
             data = response.json()
-            return data['name']
-        except requests.exceptions.RequestException as e:
-            print(f"Ошибка при получении имени: {e}")
+            return (data['forms'][0]['name'])
+        else:
             return "Pikachu"
+
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покемона: {self.name}, Здоровье: {self.health}, Атака: {self.attack}"
+        return f"""Имя твоего покеомона: {self.name}
+Cила покемона: {self.power}
+Здоровье покемона: {self.hp}"""
 
+    # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
-
-    # Метод для атаки другого покемона
-    def attack_pokemon(self, other_pokemon):
-        damage = randint(self.attack // 2, self.attack) # Случайный урон в диапазоне от половины атаки до полной атаки
-        other_pokemon.health -= damage
-        print(f"{self.name} атакует {other_pokemon.name} и наносит {damage} урона!")
-        if other_pokemon.health <= 0:
-            print(f"{other_pokemon.name} повержен!")
+    
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"""Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}
+Здоровье @{enemy.pokemon_trainer} теперь {enemy.hp}"""
         else:
-            print(f"У {other_pokemon.name} осталось {other_pokemon.health} здоровья.")
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
-    @classmethod
-    def set_base_health(cls, new_health):
-        Pokemon.BASE_HEALTH = new_health
-        print(f"Базовое здоровье покемонов изменено на {new_health}")
-
-    @classmethod
-    def set_base_attack(cls, new_attack):
-        Pokemon.BASE_ATTACK = new_attack
-        print(f"Базовая атака покемонов изменена на {new_attack}")
+class Wizard(Pokemon):
+  pass
 
 
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
